95c590af9eb131e48e9ab5130894ac837cfb25f8
Ezeobi Uchenna <78647544+jay-uchenna@users.noreply.github.com>
2022-01-18 21:36:14 +0300

    Initial Commit

---
+++ next date/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  next date
+//
+//  Created by Jedidiah Uchenna on 18.01.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
